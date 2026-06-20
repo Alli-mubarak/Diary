86380043fa680d733dc0f54f5cc50a1cbb925b14
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-20 23:23:10 +0100

    Enhance theme toggle and implement search functionality
    Refactor theme toggle functionality and add search feature.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,4 @@
-
 // Get elements from HTML
-const html = document.querySelector('html');
 const entryInput = document.getElementById('entry');
 const toggleCtrl = document.querySelector('.toggle');
 const inputBox = document.getElementById('inputBox');
@@ -8,39 +6,28 @@ const addButton = document.getElementById('add-button');
 const entriesDiv = document.getElementById('entries');
 const themeToggle = document.querySelector('.theme');
 const themeInner = document.querySelector('.t-inner');
+const searchIcon = document.getElementById('search');
+const searchForm = document.getElementById('search-form');
+const errorBox = document.getElementById('error-box');
+const searchInput = document.getElementById('search-input');
 
 
-// theme auto set
-let n = new Date().getHours();
-console.log(n);
+// toggle theme
+const html = document.querySelector('html');
 let isDark = false;
-function setDarkTheme(){
- html.setAttribute('class','dark');
-        isDark = true;
-        themeInner.style.left = '3%';
-        themeInner.style.boxShadow = '0 0 15px orange';
-        themeToggle.style.boxShadow = '0 0 0 #fff055 ';
-}
-function setLightTheme(){
-  html.removeAttribute('class','dark');
+themeToggle.onclick = () =>{
+    if(isDark){
+        html.removeAttribute('class','dark');
         isDark = false;
         themeInner.style.left = '6px';
         themeInner.style.boxShadow = '0 0 0 orange';
         themeToggle.style.boxShadow = '-2px 0 1px #fff055 ';
-}
-if(n >= 19 || n<6){
-  setDarkTheme();
-}else{
-  setLightTheme();
-}
-
-// toggle theme
-
-themeToggle.onclick = () =>{
-    if(isDark){
-        setLightTheme();
     }else{
-       setDarkTheme();
+        html.setAttribute('class','dark');
+        isDark = true;
+        themeInner.style.left = '3%';
+        themeInner.style.boxShadow = '0 0 15px orange';
+        themeToggle.style.boxShadow = '0 0 0 #fff055 ';
     }
 }
 
@@ -50,23 +37,53 @@ let isAdderOpen = false;
 toggleCtrl.onclick = () =>{
     if(!isAdderOpen){
      inputBox.style.height = 'auto';
-     inputBox.style.padding = '15px';
         isAdderOpen = true;
         toggleCtrl.style.transform = 'rotate(135deg)';
-        entriesDiv.style.height = 'calc(100dvh - 215px)';
         return 
     }
         inputBox.style.height = '0';
         isAdderOpen = false;
     toggleCtrl.style.transform = 'rotate(0deg)';
-    inputBox.style.padding = '0 15px';
-    entriesDiv.style.height = 'calc(100dvh - 130px)';
 }
-
+//search Functionality
+let isSearchBoxOpen = false;
+let searchText;
+searchIcon.onclick = () =>{
+  searchForm.reset()
+  if(!isSearchBoxOpen){
+  searchForm.style.display = 'flex'
+  isSearchBoxOpen = true
+}else{
+  searchForm.style.display = 'none'
+  isSearchBoxOpen = false
+}
+  
+}
+searchForm.addEventListener('submit',(e) => {
+  e.preventDefault()
+  searchText = searchForm.search.value;
+  getEntries(search)
+  
+})
+searchInput.addEventListener('input', () =>{
+  if(searchInput.value === ''){
+    getEntries(displayEntries)
+  }
+})
 const apiKey = 'Arrfayygjjiggjvfgjkj7534ghgcc';
 //const apiUrl = 'http://localhost:39693';
-const apiUrl = ''
+const apiUrl = 'https://diary-app-omega-lime.vercel.app'
 
+//loader Function
+function load(){
+  entriesDiv.innerHTML = ` <p class="loader"></p>
+    <p class="loader"></p>
+    <p class="loader"></p>
+    <p class="loader"></p>
+        <p class="loader"></p>
+            <p class="loader"></p>
+                <p class="loader"></p>`
+}
 
 // Function to add new entry
 inputBox.onsubmit = (e) =>{
@@ -77,7 +94,7 @@ inputBox.onsubmit = (e) =>{
     const entryText = JSON.stringify(Object.fromEntries(formData.entries())); 
  const txtL = inputBox.description.value.length
   if(txtL > 0){
-      entryInput.disabled = true;
+    entryInput.disabled = true;
 const requestOptions = {
   method: 'post',
   headers: {
@@ -94,10 +111,7 @@ fetch(addApi, requestOptions)
     for (i in data){
       st+= i+':'+ data[i];
     }
-    getEntries();
-      setTimeout(()=>{
-      entryInput.disabled = false;
-      },2000);
+    getEntries(displayEntries);
   })
   .catch(error => {
     alert(error)
@@ -113,6 +127,135 @@ fetch(addApi, requestOptions)
   const editButtons = document.getElementsByClassName('edit');
   for (let i = 0; i < editButtons.length; i++) {
     editButtons[i].addEventListener('click', editEntry);
+  }
+  }else{
+  alert('Input cannot be empty!')
+  }
+}
+
+// get all entries
+function getEntries(fn){
+  load()
+  errorBox.classList.add('hidden');
+  const getAllApi = `${apiUrl}/getEntries`;
+  const requestOptions = {
+  method: 'get',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`, // Standard practice for API keys/tokens
+    'Content-Type': 'application/json' // Specify the content type
+  }
+}
+fetch(getAllApi, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    entriesDiv.style.height = '80dvh';
+    entriesDiv.innerHTML = '';
+    fn(data)
+
+  })
+  .catch(error => {
+    alert(error)
+    console.error('Error:', error)})
+}
+
+//function that renders entries to the UI
+function displayEntries(data){
+      for (let i=data.length-1; i >=0; i--){
+      const d = new Date(data[i].createdAt);
+      const timeDate = d.toLocaleString();
+      const entryHTML = `<p id='${data[i]._id}'><b>${data[i].description}</b> <button class="edit" onclick='editEntry(this)'>Edit</button> <button class="delete" onclick='deleteEntry(this)'>Delete</button><span class='timeAdded'>${timeDate}</span></p>`;
+ entriesDiv.innerHTML += entryHTML;
+    }
+}
+// function for searching
+function search(data){
+  let entryHTML = ''
+      for (let i=data.length-1; i >=0; i--){
+      const d = new Date(data[i].createdAt);
+      const timeDate = d.toLocaleString();
+      searchText = new RegExp(searchText,"gi");
+      
+      if(data[i].description.search(searchText) > 0){
+        
+      entryHTML+= `<p id='${data[i]._id}'><b>${data[i].description}</b> <button class="edit" onclick='editEntry(this)'>Edit</button> <button class="delete" onclick='deleteEntry(this)'>Delete</button><span class='timeAdded'>${timeDate}</span></p>`;
+      }
+      }
+      entriesDiv.innerHTML = entryHTML;
+      
+      if(!entryHTML){
+   entriesDiv.style.height = '0';
+   errorBox.classList.remove('hidden');
+ }
+}
+
+// Function to delete entry
+function deleteEntry(e) {
+if(confirm('Are you sure you want to delete this entry?')){
+    entryID = e.parentNode.id
+    deleteApi = `${apiUrl}/deleteEntry/${entryID}`;
+      const requestOptions = {
+  method: 'delete',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`, // Standard practice for API keys/tokens
+    'Content-Type': 'application/json' // Specify the content type
+  }
+}
+fetch(deleteApi, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    let st = ''
+    for (i in data){
+      st += data[i]
+      
+    }
+    alert(st)
+    getEntries(displayEntries);
+  })
+  .catch(error => {
+    alert(error)
+    console.error('Error:', error)})
+}
+}
+ 
+
+function editEntry(e){
+let container = e.parentNode
+  let text =  container.children[0];
+  if(text.innerHTML.length > 0){
+  if(!container.isEdited){
+  text.setAttribute('class','highlight');
+  text.setAttribute('contenteditable','true');
+  e.innerHTML = 'Save';
+  container.isEdited = true;
+  return
+  }
+  text.removeAttribute('class','highlight');
+  e.innerHTML = 'Edit';
+  text.removeAttribute('contenteditable','true');
+  container.isEdited = false
+  
+  entryID = e.parentNode.id
+  newDescription = {description: text.innerHTML}
+    editApi = `${apiUrl}/editEntry/${entryID}`;
+      const requestOptions = {
+  method: 'post',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`, // Standard practice for API keys/tokens
+    'Content-Type': 'application/json' // Specify the content type
+  },
+  body: JSON.stringify(newDescription)
+}
+fetch(editApi, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    getEntries(displayEntries);
+  })
+  .catch(error => {
+    alert(error)
+    console.error('Error:', error)})
+}
+}
+getEntries(displayEntries);    editButtons[i].addEventListener('click', editEntry);
   }
   }else{
   alert('Input cannot be empty!')
