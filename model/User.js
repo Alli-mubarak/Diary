0852612be784e1a8aa1963d937ada07a64145220
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-22 08:47:30 +0100

    Create User schema and model with Mongoose
    Define User schema with required fields and export model.

--- model/User.js
+++ model/User.js
@@ -1,1 +1,31 @@
 
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  profilePic: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
+
+    
