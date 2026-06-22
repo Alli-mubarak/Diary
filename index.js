8b988b1b7c6391b0cad2fe30a4b63dd1f2538f86
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-22 08:41:28 +0100

    Uncomment import statements for User and add functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import connectDB from './config/db.js';
 import mongoose from 'mongoose';
 //import User from './model/User.js'; // 2. Import User Model
-//import {createEntry, getEntries, getAnEntry, updateEntry, deleteEntry} from './config/add.js';
+import {createEntry, getEntries, getAnEntry, updateEntry, deleteEntry} from './config/add.js';
 import express from 'express';
 import {Router} from 'express'
 const app = express();
