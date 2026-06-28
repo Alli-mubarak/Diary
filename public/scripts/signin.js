ea5a37fb778bf4581097fd2dc00619e0f5c9e3dd
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-28 16:24:14 +0100

    Create signin.js

---
+++ public/scripts/signin.js
@@ -0,0 +1,29 @@
+const themeToggle = document.querySelector('.theme');
+// toggle theme
+const html = document.querySelector('html');
+let isDark = false;
+themeToggle.onclick = () =>{
+    if(isDark){
+        html.removeAttribute('class','dark');
+        isDark = false;
+        themeToggle.innerHTML = `<i class="fa-solid fa-moon"></i>`
+    }else{
+        html.setAttribute('class','dark');
+        isDark = true;
+        themeToggle.innerHTML = `<i class="fa-regular fa-moon"></i>`
+    }
+}
+
+//theme autoset
+const d = new Date()
+const hour = d.getHours();
+
+if(hour > 6 && hour < 17){
+    html.removeAttribute('class','dark');
+        isDark = false;
+        themeToggle.innerHTML = `<i class="fa-solid fa-moon"></i>`
+}else{
+    html.setAttribute('class','dark');
+        isDark = true;
+        themeToggle.innerHTML = `<i class="fa-regular fa-moon"></i>`
+          }
