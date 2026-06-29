894445ac30a981d500837531140f3ecc3aea85a5
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-29 14:53:37 +0100

    Implement encrypt and decrypt functions in Crypt.js

---
+++ Utils/Crypt.js
@@ -0,0 +1,31 @@
+
+//All characters compiled in a string
+const chars = `F.,éêēèëūüúûùìïíîīóôöòœøōõàáâäāåãæßñç'"@:&~70°)r^K•B6;/PDH#_T=seXhu✓vJM(I®©?LQa¢YNm5A|8fG€inZwgx9√%W!t¥jpdU4co$2V3Rklq1y£zOEbCS F.,éêēèëūüúûùìïíîīóôöòœøōõàáâäāåãæßñç'"@:&~70°)r^K•B6;/PDH#_T=seXhu✓vJM(I®©?LQa¢YNm5A|8fG€inZwgx9√%W!t¥jpdU4co$2V3Rklq1y£zOEbCS `;
+
+function encrypt(text, salt){
+const keyDigit = chars / 2
+   const helperDigit = keyDigit % salt;
+         let encryptedText = '';
+         for (let i=0; i < word.length; i++){
+        let textToAdd = chars.charAt(chars.indexOf(word.charAt(i)) + helperDigit);
+         encryptedText += textToAdd ;
+         }
+         return encryptedText;  
+}
+function decrypt(encryptedText, salt){
+  keyDigit = chars / 2;
+         let plainText = '';
+         helperDigit = keyDigit % salt;
+         
+         for (let i = 0; i < encryptedText.length; i++){
+         const inDex = chars.indexOf(encryptedText.charAt(i)) - helperDigit;
+         if(inDex >= 0){
+     plainText += chars.charAt(inDex);
+     }else{
+         plainText +=chars.charAt(inDex + keyDigit);
+         
+     }
+         }
+     return plainText;
+         }
+export {encrypt, decrypt}
