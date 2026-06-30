edc958908ddfe737c31be27ca7cfa1af03c003e2
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-06-30 07:10:39 +0100

    Change username to displayName in user creation

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ app.post('/api/sign-up', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ username, email, password: hashedPassword });
+    const newUser = new User({ displayName: username, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: 'Registration successful!' });
