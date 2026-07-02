476ed5f63b54a87bf0ff775b21182601e815a700
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-07-02 19:05:12 +0100

    Fix formatting of newUser object creation

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ app.post('/api/sign-up', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ displayName: username, email, password: hashedPassword, profilePic: "/user.png", googleId: ""});
+    const newUser = new User({ displayName: username, email, password: hashedPassword, profilePic: "/user.png"});
     await newUser.save();
     // Log the user in automatically
     // Convert the Mongoose document to a plain JavaScript object
