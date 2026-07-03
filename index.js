4e620b8ea60b3ff0d093fbcd562c90b688e47266
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-07-03 05:37:18 +0100

    Update error messages for user authentication

--- index.js
+++ index.js
@@ -139,13 +139,13 @@ passport.use(new LocalStrategy(
             // 1. Find the user by email
             const user = await User.findOne({ email: email });
             if (!user) {
-                return done(null, false, { message: 'Incorrect email or password.' });
+                return done(null, false, { message: 'User not found!.' });
             }
 
             // 2. Validate password (assuming you hash passwords on signup)
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
-                return done(null, false, { message: 'Incorrect email or password.' });
+                return done(null, false, { message: 'Incorrect credentials.' });
             }
 
             // 3. Success
